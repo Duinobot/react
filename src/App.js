b757b46e1be88182ed77a7cd00cb23a367232fb4
Dan Mo <danmo2060@gmail.com>
2021-08-23 14:57:58 +1000

    Co

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import logo from './logo.svg';
+
 import { Navbar, NavbarBrand } from "reactstrap";
 import './App.css';
+import Menu from './components/MenuComponent';
+
 
 function App() {
   return (
@@ -13,8 +15,7 @@ function App() {
 
         </div>
       </Navbar>
-
-
+      <Menu />
     </div>
   );
 }
